eddf32b88b2710cfc40394494e6d2817829b6cdf
Wes Ketchum <wesleyketchum@gmail.com>
2013-07-22 15:22:59 -0400

    added item object

---
+++ lib/item.rb
@@ -0,0 +1,9 @@
+class Item
+
+  def initialize(item_hash)
+    @name = item_hash.title
+    @price = item_hash.item_price.amount,
+    @sku = item_hash.seller_sku,
+    @quantity = item_hash.quantity_ordered
+  end
+end
