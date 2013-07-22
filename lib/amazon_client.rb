545df17c50111091186831d77f1f6dab1cb899a8
Wes Ketchum <wesleyketchum@gmail.com>
2013-07-22 10:35:49 -0400

    removed @base_response

--- lib/amazon_client.rb
+++ lib/amazon_client.rb
@@ -10,12 +10,11 @@ class AmazonClient
   end
 
   def get_orders
-    response = @base_response
     statuses = ['Unshipped', 'PartiallyShipped', 'Shipped']
     order_list = @client.orders.list_orders(last_updated_after: @last_updated, order_status: statuses)
 
     if order_list.orders.nil?
-      response
+      nil
     elsif order_list.orders.is_a? MWS::API::Response
       assemble_response([order_list.orders])
     else
@@ -57,15 +56,7 @@ class AmazonClient
             status: order.order_status,
             email: order.buyer_email,
             line_items: [],
-            shipping_address: {
-              firstname: order.buyer_name.split(' ').first,
-              lastname: order.buyer_name.split(' ').last,
-              address1: order.shipping_address.address_line1,
-              city: order.shipping_address.city,
-              zipcode: order.shipping_address.postal_code,
-              phone: order.shipping_address.phone,
-              country: order.shipping_address.country_code,
-              state: order.shipping_address.state_or_region },
+            shipping_address: address_hash(order),
             shipments: {
               shipping_method: order.shipment_service_level_category,
               items: [] }}}}
@@ -76,5 +67,16 @@ class AmazonClient
       price: item.item_price.amount,
       sku: item.seller_sku,
       quantity: item.quantity_shipped }
+  end
+
+  def address_hash(order)
+    { firstname: order.buyer_name.split(' ').first,
+      lastname: order.buyer_name.split(' ').last,
+      address1: order.shipping_address.address_line1,
+      city: order.shipping_address.city,
+      zipcode: order.shipping_address.postal_code,
+      phone: order.shipping_address.phone,
+      country: order.shipping_address.country_code,
+      state: order.shipping_address.state_or_region }
   end
 end
