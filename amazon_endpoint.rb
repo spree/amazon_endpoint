46a99d0c97bd4203ded35602342138dc1557ab8d
Wes Ketchum <wesleyketchum@gmail.com>
2013-10-18 11:03:53 -0400

    rough outline of confirm_shipment action

--- amazon_endpoint.rb
+++ amazon_endpoint.rb
@@ -38,6 +38,20 @@ class AmazonEndpoint < EndpointBase
     process_result code, merged_response(response)
   end
 
+  post '/confirm_shipment' do
+    feed = AmazonFeed.new(config)
+    @base_response = { message_id: @message[:message_id] }
+
+    begin
+      id = feed.submit(@message)
+      code = 200
+    rescue => e
+      code, response = handle_error(e)
+    end
+
+    process_result code, merged_response(response)
+  end
+
   private
 
   def handle_error(e)
