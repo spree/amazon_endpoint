6c06bc9580d2e5464bff8d4ee0edb81ab7f99db4
Pablo Cantero <pablo@pablocantero.com>
2013-10-25 12:12:38 -0200

    Removes unnecessary `if` for state abbr exceptions

--- lib/order.rb
+++ lib/order.rb
@@ -170,8 +170,6 @@ class Order
                    'AP'   => 'U.S. Armed Forces – Pacific',
                    'D.C.' => 'District Of Columbia' }
 
-    return exceptions[state_abbr] if exceptions.key?(state_abbr)
-
     exceptions[state_abbr] || ModelUN.convert_state_abbr(state_abbr)
   end
 end
